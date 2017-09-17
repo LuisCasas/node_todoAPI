440d5d6bc65cd653ab08d4aec17044b7804d9f2b
Luis <luis@CashBrokerss-MacBook-Pro.local>
2017-09-17 11:38:48 +0100

    Add POST /todos routes

---
+++ server/models/todo.js
@@ -0,0 +1,60 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    completedAt: {
+        type: Number,
+        default: null
+    }
+});
+
+module.exports = {Todo}
+
+/*
+var anotherTodo = new Todo({
+    text: 'Work at Adobe',
+    completed: false
+});
+
+anotherTodo.save().then((doc) => {
+    console.log('Todo saved', doc);
+}, (e) => {
+    console.log('Error');
+});
+
+
+mongoose.Promise = global.Promise;  
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String
+    }, 
+    completed: {
+        type: Boolean
+    },
+    completedAt: {
+        type: Number
+    }
+});
+
+var newTodo = new Todo({
+    text: 'Study Programming'
+});
+
+newTodo.save().then((doc) => {
+    console.log('Saved todo', doc);
+}, (e) => {
+    console.log('error');
+});
+
+*/
