7f1f03ca36170672b638e4be9853d49b47bd5390
LuisCasas <sc.luis@gmail.com>
2017-10-14 16:46:13 +0100

    Add GET /users/me and auth middleware

--- server/server.js
+++ server/server.js
@@ -3,11 +3,12 @@ require('./config/config.js')
 const _ = require('lodash'); 
 const express = require('express');
 const bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-var {ObjectID} = require('mongodb');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -118,6 +119,11 @@ app.post('/users', (req, res) => {
         res.status(400).send(e);
     });    
 });
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+})
 
 app.listen(port, () => {
     console.log('Express up and running at port', port);
